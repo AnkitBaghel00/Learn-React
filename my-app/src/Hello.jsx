61f3b91426a562f6724c2877f4448f90a759306d
baghe <baghelankit602@gmail.com>
2025-11-27 23:35:02 +0530

    1. project structure, 2. component and JSX 3. css styling of compenent

---
+++ my-app/src/Hello.jsx
@@ -0,0 +1,23 @@
+
+function Hello(){
+    const name = "ankit"
+    const headingStyle = {
+        color:"red",
+        textAlign:"center",
+        backgroundColor:"pink",
+        fontSize:50 
+    }
+    return (
+        <>
+        <h2 style={{  color:"red",textAlign:"center",}}> Hello {name}!</h2>
+        <p className='test'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea minus, quibusdam sit cumque voluptatem sapiente enim molestiae qui, eaque ipsa nihil cum sunt dolor unde fuga atque tempore, officiis esse.</p>
+        </>
+    )
+
+}
+
+
+export default Hello    
+
+
+
